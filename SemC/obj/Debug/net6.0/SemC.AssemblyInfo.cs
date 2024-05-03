e60e1f1a067ebf0478f8e76917b02c1a24bd85d2
ScholzovaLucie <lucka.scholz@gmail.com>
2024-05-03 07:31:21 +0200

    update

--- SemC/obj/Debug/net6.0/SemC.AssemblyInfo.cs
+++ SemC/obj/Debug/net6.0/SemC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a42e633c9478ea5f08c5909c9c8b76ea3b8ba6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
